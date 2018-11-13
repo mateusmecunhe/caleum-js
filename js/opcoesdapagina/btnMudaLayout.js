f8cb6e6cdf85405e40dbec1d045795d6617ec87c
Mateus <mateusmecunhe@gmail.com>
2018-11-12 22:47:28 -0200

    primeira aula

---
+++ js/opcoesdapagina/btnMudaLayout.js
@@ -0,0 +1,24 @@
+var btn = document.querySelector("#btnMudaLayout")
+
+function mudaTexto(){
+/* btn.textContent=btn.textContent==="Linhas"?"Colunas":"Linhas" */
+/* esse código acima substitui todas as linhas abaixo dentro da função. É igual o if do excel */
+    if(btn.textContent.trim()==="Linhas"){
+        btn.textContent = "Blocos"
+    } else {
+        btn.textContent = "Linhas"
+    }
+}
+
+
+function mudaLayout(){
+
+    var layoutCartoes = document.querySelector(".mural")
+    layoutCartoes.classList.toggle("mural--linha")
+}
+
+btn.addEventListener("click", function(){
+    mudaTexto()
+    mudaLayout()
+}
+)
